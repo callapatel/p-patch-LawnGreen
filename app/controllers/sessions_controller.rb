81265fed75b844f179007d18b4ddb1fc2ea341e9
Calla <callapatel@gmail.com>
2014-12-03 13:17:10 -0800

    validations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,8 @@
 class SessionsController < ApplicationController
   def create
-    @uid = request.env["omniauth.auth"]["uid"]
+    @uid = request.env["omniauth.auth"]["user_id"]
     auth_hash = request.env['omniauth.auth']
+    raise
     redirect_to "/"
   end
 end
