a099463cf73faadb977ea4e121eeb344268c129e
Crystal Perreira <cvperreira@gmail.com>
2014-12-02 16:06:11 -0800

    fix omniauth file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
-ails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, YOUR_CLIENT_ID, YOUR_CLIENT_SECRET
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, ENV['TWITTER_CLIENT_ID'], ENV['TWITTER_CLIENT_SECRET']
 end
