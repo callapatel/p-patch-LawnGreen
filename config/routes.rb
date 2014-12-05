18b364130bab8f29f4ed4f701b6d8308050598ba
Calla <callapatel@gmail.com>
2014-12-05 15:15:08 -0800

    fixed weather path

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   get "/post/new",                   to: "posts#new",           as: :new_post
   post "/posts",                      to: "posts#create",        as: :posts
   get "/posts",                       to: "posts#index"
+  get "/posts/:id" ,                  to: "posts#show",         as: :post
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
