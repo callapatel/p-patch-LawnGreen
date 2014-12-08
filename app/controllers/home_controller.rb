5068ba4d4bf912a4c50ee5edf76f117c8cd8925f
Calla <callapatel@gmail.com>
2014-12-08 10:40:53 -0800

    cal incremental

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,15 +5,29 @@ require 'date'
 
   def index
     current_month = Time.now.month
+    #@holder = Time.now
     current_year = Time.now.year
     calend = Calendar.new
-    @month = calend.grab_month(current_year, current_month)
-    @calendar = calend.date_layover(@month)
-    @week1 = @calendar[0..6]
-    @week2 = @calendar[7..13]
-    @week3 = @calendar[14..20]
-    @week4 = @calendar[21..27]
-    @week5 = @calendar[28..34]
+    if params[:thing] == 'next'
+      x = Date.parse(params[:time])
+      @month = calend.grab_month(current_year, x.next_month.month)
+      @calendar = calend.date_layover(@month)
+      @week1 = @calendar[0..6]
+      @week2 = @calendar[7..13]
+      @week3 = @calendar[14..20]
+      @week4 = @calendar[21..27]
+      @week5 = @calendar[28..34]
+      params[:thing] = nil
+    else
+      @month = calend.grab_month(current_year, current_month)
+      @calendar = calend.date_layover(@month)
+      @week1 = @calendar[0..6]
+      @week2 = @calendar[7..13]
+      @week3 = @calendar[14..20]
+      @week4 = @calendar[21..27]
+      @week5 = @calendar[28..34]
+
+    end
   end
 
   def weather
@@ -23,6 +37,6 @@ require 'date'
     render :partial => "weather"
     #render :partial => "weather", :locals => {myName: @myThing}
   end
-  
+
 
 end
