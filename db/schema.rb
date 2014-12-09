cc114c83f76aed0461818255f97a8f8b46ba3e13
Calla <callapatel@gmail.com>
2014-12-09 15:46:35 -0800

    rollback comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141208175052) do
+ActiveRecord::Schema.define(version: 20141208174152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
