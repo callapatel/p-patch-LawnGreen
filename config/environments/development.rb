9e3f98ded0308b669f79c8f1bbd644a8222fdd20
Calla <callapatel@gmail.com>
2014-12-09 15:11:04 -0800

    nothing relevant

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -44,7 +44,7 @@ Rails.application.configure do
     user_name:            ENV["MAIL_USERNAME"], # your gmail login
     password:             ENV["MAIL_PASSWORD"], # your gmail password
     authentication:       'plain',
-    enable_starttls_auto: true  
-  } 
+    enable_starttls_auto: true
+  }
 
 end
