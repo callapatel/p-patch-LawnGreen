c6c7fd2cb2c4d72957e7b86504fec01ac9ffeab0
Calla <callapatel@gmail.com>
2014-12-11 13:54:57 -0800

    email and photos

--- app/mailers/news_mailer.rb
+++ app/mailers/news_mailer.rb
@@ -1,15 +1,19 @@
 class NewsMailer < ActionMailer::Base
-  default from: "mcomcalla@gmail.com"
+
 
   def welcome(user_id)
     @user = User.find(user_id)
-    mail(to: @user.email, subject: "welcome!")
+    mail :subject => "welcome to lease the life",
+    :to => @user.email,
+    :from =>  "LTLteam@leasethelife.com"
   end
 
   def posts(user, post)
     @user = user
     @post = post
-    mail(to: @user.email, subject: "hello #{@post.title}" )
+    mail :subject => "hello #{@post.title}",
+    :to => @user.email,
+    :from => "LTLteam@leasethelife.com"
 
   end
 end
