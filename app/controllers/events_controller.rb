23a30e8151585082a036e4b421bcc0430deb475b
Crystal Perreira <cvperreira@gmail.com>
2014-12-12 11:25:49 -0800

    events show on calendar

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,11 +5,17 @@ require 'date'
 
   def new
     @event = Event.new(params.require(:event).permit(:title, :description, :date))
+    @event.date = convert_date(@event.date)
     if @event.save
       redirect_to root_path
     else
       render :new
     end
+  end
+
+  def convert_date(string)
+    new_date = string[6..9]+'-'+string[0..1]+'-'+string[3..4]
+    new_date
   end
 
 end
